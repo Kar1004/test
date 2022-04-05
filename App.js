2a9e3fbb589e3666545282cf9bbf6b43b1fa7861
Kar1004 <97450951+Kar1004@users.noreply.github.com>
2022-04-05 16:00:39 +0200

    jour1 react-native

--- App.js
+++ App.js
@@ -1,21 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image ,Pressable, ScrollView,Alert , ActivityIndicator} from 'react-native';
+
+
 
 export default function App() {
+
+  const ButtonAlert = () =>
+  Alert.alert(
+   "M'aimez vous ?",
+  "Si vous dites noN,vous allez souffrir ?",
+    [
+      {
+        text: "Oui",
+        onPress: () => console.log("hELLO"),
+       
+      },
+      { text: "Non", onPress:ButtonAlert }
+    ]
+  );
+
+
   return (
+    <ScrollView style={styles.scroll}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.container2}>
+        <Text style={styles.title1}>
+           Text
+        </Text>
+        <Text style={styles.title2}>
+          test2
+          </Text>
+            <Text style={styles.title3} >
+          TEST3
+          </Text>
+      </View>
+      <Image
+  source={{ uri: "https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png" }}
+  style={{ width: 240, height: 100 }} />
+      <Image
+  source={{ uri: "https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png" }}
+  style={{ width: 240, height: 100 }} />
+
+   <Pressable onPress={ButtonAlert}>
+    <Text>HELLOooooooooOOOOOOOOOOO</Text>
+     
+   </Pressable>
+   <ActivityIndicator size="large" color="#00ff00" />
+   
+
     </View>
+   
+  </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
+    backgroundColor: 'blue',
+   
+   
   },
-});
+
+  container2:{
+    backgroundColor: 'white',
+
+   
+
+  },
+  
+  title1 : {  
+    marginRight: 80 , 
+    marginLeft : 80,
+     fontSize:30,
+  
+  },
+
+  title2 : {
+    marginRight: 80 , 
+    marginLeft : 80,
+     fontSize:30,
+    textAlign:'center',
+  },
+
+  title3 : {
+    marginRight: 80 , 
+    marginLeft : 80,
+     fontSize:30,
+     fontWeight:'bold'
+   
+
+  }
+  
+
+});  
+
+
+
+
+
+
+
